4ba434be9a0e56f45e595cc3f716e92592f2bbe7
Elisa Uhura <elisa@uhura.cc>
2023-09-13 17:52:43 -0700

    Adjust ports

--- Program.cs
+++ Program.cs
@@ -3,14 +3,11 @@ var builder = WebApplication.CreateBuilder(args);
 // use port 80 and 443 if not in development
 if (!builder.Environment.IsDevelopment())
 {
-    builder.WebHost.UseUrls("http://*:80", "https://*:443");
+    builder.WebHost.UseUrls("http://*:8080");
 }
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World");
-
-app.UseHttpsRedirection();
-app.UseAuthorization();
 
 app.Run();
